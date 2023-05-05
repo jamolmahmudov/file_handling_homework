8dd790773b528723d1fc6d9d8fe968761b36f402
jamolmahmudov <jamolmahmudov27102003@gmail.com>
2023-05-05 09:35:56 +0500

    7

--- file07.py
+++ file07.py
@@ -6,5 +6,22 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    a=0
+    for i in data:
+        if i.isdigit():
+            a+=int(i)
+            
+    return a
+data=open("txt_file/data07.txt")
+r=data.read()
+print(main(data=r))
+#     n = 0
+#     for i in data:
+#         if i.isdigit():
+#             n += int(i)
+#     return n
+# data=open('txt_file/data07.txt')
+# r=(data).read()
+
+# print(main(data=r))
 # Read data from file
