51c20c4ba7f34cde1d73806a0f547455b836a0f1
jamolmahmudov <jamolmahmudov27102003@gmail.com>
2023-05-05 07:33:16 +0500

    4

--- file04.py
+++ file04.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    list=[]
+    for i in data:
+        if  i.isalpha():
+            list.append(i)
+    return list
+data=open("txt_file/data04.txt")
+r=(data).read()
+print(main(data=r))    
+
 # Read data from file
